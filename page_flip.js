731261df5899c7fedb7604cf29c0f6efc6476d32
Gabriel Carvalho <gabrielassis170@gmail..com>
2024-09-16 22:11:25 -0300

    first commit

---
+++ page_flip.js
@@ -0,0 +1,9 @@
+import PageFlip from 'node_modules\page-flip\dist\js\page-flip.browser.js';
+
+const pageFlip = new PageFlip(document.getElementById('revista'), {
+    width: 1024, 
+    height: 768, 
+    size: 'fixed'
+
+});
+pageFlip.loadFromImages(['2.jpg', '3.jpg', '4.jpg' ]);
